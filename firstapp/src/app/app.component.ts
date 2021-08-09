c38dd1a11199404fa66a18d8ef7871a1ad61a18c
mobaidalikhan <mobaidalikhan@gmail.com>
2021-08-09 18:37:35 -0400

    Components

--- firstapp/src/app/app.component.ts
+++ firstapp/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css']
+  // template: `<h1>Hello {{name}}</h1>`,
+  // template: `
+  //  <div>
+  //     <h1>{{appTitle}}</h1>
+  //     <div>To Tutorials Point</div>
+  //  </div>`
+
+  template: ` <div>
+      <h1>{{appTitle}}</h1>
+      <div>To Tutorials Point</div>
+   </div> `,
 })
 export class AppComponent {
-  title = 'Hello World!!!';
+  name = 'USA';
+  appTitle: string = 'Welcome';
 }
